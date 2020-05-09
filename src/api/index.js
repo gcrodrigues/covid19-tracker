1fa250be7619205ce8acceb8bcb061ec3e2b36c1
Gustavo <gcrodrigues413@gmail.com>
2020-05-09 01:49:05 -0300

    Request for daily data and countries

--- src/api/index.js
+++ src/api/index.js
@@ -18,3 +18,30 @@ export const fetchData = async () => {
     console.log(error);
   }
 }
+
+export const fetchDailyData = async () => {
+  try{
+    const { data } = await axios.get(`${baseUrl}/daily`);
+    
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate
+    }));
+    
+    return modifiedData;
+  }catch(error){
+    console.log(error)
+  }
+}
+
+export const fetchCountries = async () =>{
+  try{
+    const { data:  { countries } } = await axios.get(`${baseUrl}/countries`);
+  
+    return countries.map(country => country.name);
+  }catch(error){
+    console.log(error);
+  }
+
+}
