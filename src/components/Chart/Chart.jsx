c9f59ad1aacfb2f00ec1b0dadbd2641718f7a885
Gustavo <gcrodrigues413@gmail.com>
2020-05-09 01:57:10 -0300

    Line chart development

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line } from 'react-chartjs-2';
+
+import styles from './Chart.module.css';
 
 const Chart = () => {
-  return <h1>Chart</h1>
+  const [dailyData, setDailyData] = useState([]);
+  useEffect(() => {
+    const fetchApi = async () => {
+      setDailyData(await fetchDailyData());
+    }
+   
+    fetchApi();
+  }, []);
+
+  const lineChart = (
+    dailyData.length // 0 - false
+      ?(<Line
+        data={{
+          labels: dailyData.map(({ date }) => date),
+          datasets: [
+            {
+              data: dailyData.map(({confirmed}) => confirmed),
+              label: 'Infected',
+              borderColor: '#3333ff',
+              fill: true
+            },
+            {
+              data: dailyData.map(({deaths}) => deaths),
+              label: 'Deaths',
+              borderColor: '#ff3333',
+              backgroundColor: 'rgba(255,0,0,.5)',
+              fill: true
+            }
+          ]
+        }}
+      />) : null
+  )
+
+  return(
+    <div className={styles.container}>
+      {lineChart}
+    </div>
+  )
 }
 
 export default Chart;
